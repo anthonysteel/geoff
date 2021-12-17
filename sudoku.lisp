834b4fe48c81a927d175c17832e2c897c38696e1
Anthony Steel <13as261@queensu.ca>
2021-12-17 14:59:58 -0500

    Improve solve procedure, still incorrect

--- sudoku.lisp
+++ sudoku.lisp
@@ -22,57 +22,42 @@
     (flatten (loop for j from 0 to 2
 		   collect (nth (+ (+ i (* (floor (/ i 3)) 6)) (* j 3)) triples)))))
 
-(defun get-all-squares (board)
-  (loop for i from 0 to 8
-	collect (get-square i board)))
+(defun get-all-squares (board) (loop for i from 0 to 8 collect (get-square i board)))
 
-(defun get-row (i board)
-  (cond ((> i 8) nil)
-	((= i 0) (car board))
-	(t (get-row (- i 1) (cdr board)))))
+(defun get-row (i board) (nth i board))
 
-(defun get-col (i board)
-  (loop for row in board
-	collect (nth i row)))
+(defun get-col (i board) (loop for row in board collect (nth i row)))
 
-(defun get-all-rows (board)
-  (loop for i from 0 to 8
-	collect (get-row i board)))
+(defun get-all-rows (board) (loop for i from 0 to 8 collect (get-row i board)))
 
-(defun get-all-cols (board)
-  (loop for i from 0 to 8
-	collect (get-col i board)))
+(defun get-all-cols (board) (loop for i from 0 to 8 collect (get-col i board)))
 
-(defun duplicatep (l)
+(defun duplicate? (l)
   (cond ((null l) nil)
 	((and (> (car l) 0) (member (car l) (cdr l))) t)
-	(t (duplicatep (cdr l)))))
+	(t (duplicate? (cdr l)))))
 
-(defun not-duplicatep (l)
-  (not (duplicatep l)))
+(defun not-duplicate? (l) (not (duplicate? l)))
   
-(defun is-valid (board)
+(defun is-valid? (board)
   (let ((rows (get-all-rows board))
 	(cols (get-all-cols board))
 	(squares (get-all-squares board)))
-    (every #'identity (mapcar 'not-duplicatep (append rows cols squares)))))
+    (every #'identity (mapcar 'not-duplicate? (append rows cols squares)))))
 
-(defun is-not-valid (board)
-  (not (is-valid board)))
+(defun is-not-valid? (board) (not (is-valid? board)))
 
-(defun is-list-complete (l)
+(defun is-list-complete? (l)
   (equal (loop for i from 1 to 9 collect i)
 	 (sort (copy-list l) (lambda (x y) (< x y)))))
 
-(defun is-complete (board)
+(defun is-complete? (board)
   (let ((rows (get-all-rows board))
 	(cols (get-all-cols board))
 	(squares (get-all-squares board)))
-    (every #'identity (mapcar 'is-list-complete (append rows cols squares)))))
+    (every #'identity (mapcar 'is-list-complete? (append rows cols squares)))))
 
-(defun is-solved (board)
-  (and (is-complete board)
-       (is-valid board)))
+(defun is-solved? (board) (and (is-complete? board) (is-valid? board)))
 
 (defun print-board (board)
   (loop for row in board
@@ -81,25 +66,19 @@
 		    (write-string " "))
 	   (terpri)))
 
+(defun get-elem (i j board) (nth j (nth i board)))
+
+(defun set-elem (i j new-elem board) (setf (nth j (nth i board)) new-elem))
+
 (defun solve (board)
   (print-board board)
   (terpri)
-  (let ((new-board (copy-board board)))
-    (loop named outer for i from 0 to 8
-	  do (loop for j from 0 to 8
-		   do (if (= (nth j (nth i new-board)) 0)
-			  (loop for k from 1 to 9
-				do (setf (nth j (nth i new-board)) k)
-				   (cond ((is-solved new-board) (return-from outer new-board))
-					 ((is-valid new-board) (solve new-board)))))))))
-
-(defun solve (board)
-  (let ((new-board (make-new-board board)))
-    (loop for row in board
-	  do (loop for entry in row
-		   do (if (= entry 0)
-			  (loop for i from 1 to 9
-				do (progn (setf entry i)
-					  (cond ((is-solved new-board) (return new-board))
-						((is-valid new-board) (solve new-board))
-						(t (return))))))))))
+  (cond ((is-solved? board) board)
+	((is-valid? board)
+	 (let ((new-board (copy-board board)))
+	   (loop for i from 0 to 8
+		 do (loop for j from 0 to 8
+			  do (when (= (get-elem i j new-board) 0)
+			       (loop for k from 1 to 9
+				     do (set-elem i j k new-board)
+					(solve new-board)))))))))
