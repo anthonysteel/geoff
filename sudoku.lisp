6e8bad3f2c6245440dd594ad47a9c88348fd7807
Anthony Steel <13as261@queensu.ca>
2021-12-21 10:14:28 -0500

    Remove C program and implement LISP procedures to solve sudoku

--- sudoku.lisp
+++ sudoku.lisp
@@ -1,84 +1,68 @@
-(defun copy-board (board)
+(defun copy (board)
   (loop for row in board
 	collect (loop for entry in row
 		      collect entry)))
 
-(defun flatten (board)
-  (if (null board)
-      nil
-      (append (car board) (flatten (cdr board)))))
+(defun get-rows-from (i j board)
+  (loop for num from i to j
+	collect (nth num board)))
 
-(defun make-triples (l)
-  (defun make-triples-iter (i l)
-    (if (= i 0)
-	nil
-	(cons (car l) (make-triples-iter (- i 1) (cdr l)))))
-  (if (null l)
-      nil
-      (cons (make-triples-iter 3 l) (make-triples (cdddr l)))))
+(defun get-col (i board)
+  (loop for row in board
+	collect (nth i row)))
 
-(defun get-square (i board)
-  (let ((triples (make-triples (flatten board))))
-    (flatten (loop for j from 0 to 2
-		   collect (nth (+ (+ i (* (floor (/ i 3)) 6)) (* j 3)) triples)))))
+(defun get-all-cols (board)
+  (loop for i from 0 to 8
+	collect (get-col i board)))
 
-(defun get-all-squares (board) (loop for i from 0 to 8 collect (get-square i board)))
+(defun get-square (i j board)
+  (loop for row in (get-rows-from (* i 3) (+ (* i 3) 2) board)
+	append (loop for k from 0 to 2
+		     collect (nth (+ k (* j 3)) row))))
 
-(defun get-row (i board) (nth i board))
+(defun get-all-squares (board)
+  (loop for i from 0 to 2
+	append (loop for j from 0 to 2
+		     collect (get-square i j board))))
 
-(defun get-col (i board) (loop for row in board collect (nth i row)))
+(defun get-all-rows-cols-squares (board)
+  (append board (get-all-cols board) (get-all-squares board)))
 
-(defun get-all-rows (board) (loop for i from 0 to 8 collect (get-row i board)))
-
-(defun get-all-cols (board) (loop for i from 0 to 8 collect (get-col i board)))
-
-(defun duplicate? (l)
+(defun has-duplicates? (l)
   (cond ((null l) nil)
-	((and (> (car l) 0) (member (car l) (cdr l))) t)
-	(t (duplicate? (cdr l)))))
+	((= (first l) 0) (has-duplicates? (rest l)))
+	(t (or (not (null (member (first l) (rest l)))) (has-duplicates? (rest l))))))
 
-(defun not-duplicate? (l) (not (duplicate? l)))
-  
+(defun list-and (l)
+  (cond ((null l) t)
+	(t (and (first l) (list-and (rest l))))))
+
 (defun is-valid? (board)
-  (let ((rows (get-all-rows board))
-	(cols (get-all-cols board))
-	(squares (get-all-squares board)))
-    (every #'identity (mapcar 'not-duplicate? (append rows cols squares)))))
-
-(defun is-not-valid? (board) (not (is-valid? board)))
-
-(defun is-list-complete? (l)
-  (equal (loop for i from 1 to 9 collect i)
-	 (sort (copy-list l) (lambda (x y) (< x y)))))
+  (list-and (loop for l in (get-all-rows-cols-squares board)
+		  collect (not (has-duplicates? l)))))
 
 (defun is-complete? (board)
-  (let ((rows (get-all-rows board))
-	(cols (get-all-cols board))
-	(squares (get-all-squares board)))
-    (every #'identity (mapcar 'is-list-complete? (append rows cols squares)))))
-
-(defun is-solved? (board) (and (is-complete? board) (is-valid? board)))
+  (let ((complete '(1 2 3 4 5 6 7 8 9)))
+    (list-and (loop for l in (get-all-rows-cols-squares board)
+		    collect (equal (sort (copy-list l) #'<)
+				   complete)))))
 
 (defun print-board (board)
   (loop for row in board
 	do (loop for num in row
 		 do (write num)
 		    (write-string " "))
-	   (terpri)))
-
-(defun get-elem (i j board) (nth j (nth i board)))
-
-(defun set-elem (i j new-elem board) (setf (nth j (nth i board)) new-elem))
+	   (terpri))
+  (terpri))
 
 (defun solve (board)
-  (print-board board)
-  (terpri)
-  (cond ((is-solved? board) board)
-	((is-valid? board)
-	 (loop for i from 0 to 8
-	       do (loop for j from 0 to 8
-			do (when (= (get-elem i j board) 0)
-			     (loop for k from 1 to 9
-				   do (let ((new-board (copy-board board)))
-					(set-elem i j k new-board)
-					(solve new-board)))))))))
+  (cond ((is-complete? board) (print-board board))
+	(t (loop named outer for i from 0 to 8
+		 do (loop for j from 0 to 8
+			  do (if (= (nth j (nth i board)) 0)
+				 (progn (loop for num from 1 to 9
+					     do (let ((new-board (copy board)))
+						  (setf (nth j (nth i new-board)) num)
+						  (if (is-valid? new-board)
+						      (solve new-board))))
+					(return-from outer))))))))
