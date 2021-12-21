e0542991c5536378462be71cd714ab1b109c773f
Anthony Steel <13as261@queensu.ca>
2021-12-21 10:28:46 -0500

    Clean up procedures and clarify procedure names

--- sudoku.lisp
+++ sudoku.lisp
@@ -3,50 +3,6 @@
 	collect (loop for entry in row
 		      collect entry)))
 
-(defun get-rows-from (i j board)
-  (loop for num from i to j
-	collect (nth num board)))
-
-(defun get-col (i board)
-  (loop for row in board
-	collect (nth i row)))
-
-(defun get-all-cols (board)
-  (loop for i from 0 to 8
-	collect (get-col i board)))
-
-(defun get-square (i j board)
-  (loop for row in (get-rows-from (* i 3) (+ (* i 3) 2) board)
-	append (loop for k from 0 to 2
-		     collect (nth (+ k (* j 3)) row))))
-
-(defun get-all-squares (board)
-  (loop for i from 0 to 2
-	append (loop for j from 0 to 2
-		     collect (get-square i j board))))
-
-(defun get-all-rows-cols-squares (board)
-  (append board (get-all-cols board) (get-all-squares board)))
-
-(defun has-duplicates? (l)
-  (cond ((null l) nil)
-	((= (first l) 0) (has-duplicates? (rest l)))
-	(t (or (not (null (member (first l) (rest l)))) (has-duplicates? (rest l))))))
-
-(defun list-and (l)
-  (cond ((null l) t)
-	(t (and (first l) (list-and (rest l))))))
-
-(defun is-valid? (board)
-  (list-and (loop for l in (get-all-rows-cols-squares board)
-		  collect (not (has-duplicates? l)))))
-
-(defun is-complete? (board)
-  (let ((complete '(1 2 3 4 5 6 7 8 9)))
-    (list-and (loop for l in (get-all-rows-cols-squares board)
-		    collect (equal (sort (copy-list l) #'<)
-				   complete)))))
-
 (defun print-board (board)
   (loop for row in board
 	do (loop for num in row
@@ -54,6 +10,47 @@
 		    (write-string " "))
 	   (terpri))
   (terpri))
+
+(defun get-cols (board)
+  (loop for i from 0 to 8
+	collect (loop for row in board
+		      collect (nth i row))))
+
+(defun get-rows-from (i j board)
+  (loop for num from i to j
+	collect (nth num board)))
+
+(defun get-square (i j board)
+  (loop for row in (get-rows-from (* i 3) (+ (* i 3) 2) board)
+	append (loop for k from 0 to 2
+		     collect (nth (+ k (* j 3)) row))))
+
+(defun get-squares (board)
+  (loop for i from 0 to 2
+	append (loop for j from 0 to 2
+		     collect (get-square i j board))))
+
+(defun get-rows-cols-squares (board)
+  (append board (get-all-cols board) (get-all-squares board)))
+
+(defun has-duplicates? (l)
+  (cond ((null l) nil)
+	((= (first l) 0) (has-duplicates? (rest l))) ;; ignore repeated 0's
+	(t (or (not (null (member (first l) (rest l)))) (has-duplicates? (rest l))))))
+
+(defun reduce-and (l)
+  (cond ((null l) t)
+	(t (and (first l) (list-and (rest l))))))
+
+(defun is-valid? (board)
+  (reduce-and (loop for l in (get-rows-cols-squares board)
+		  collect (not (has-duplicates? l)))))
+
+(defun is-complete? (board)
+  (let ((complete '(1 2 3 4 5 6 7 8 9)))
+    (reduce-and (loop for l in (get-rows-cols-squares board)
+		    collect (equal (sort (copy-list l) #'<)
+				   complete)))))
 
 (defun solve (board)
   (cond ((is-complete? board) (print-board board))
